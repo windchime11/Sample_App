491caf0b1e026b4c3562d033c249d8ced197178e
John Z <east54john@gmail.com>
2011-11-10 12:26:06 -0500

    removing public/index.html

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,13 @@
 module ApplicationHelper
+  
+  def myhelper
+    base_title = "Ruby on Rails Tutorial Sample App"
+    if@title.nil?
+      base_title
+    else
+      "#{base_title} | #{@title}"
+    end
+end
+
+
 end
--- app/helpers/pages_helper.rb
+++ app/helpers/pages_helper.rb
@@ -1,2 +1,7 @@
 module PagesHelper
+
+  def helper_in_pages
+    "Helper in Pages are available to views as well!"
+  end
+
 end
