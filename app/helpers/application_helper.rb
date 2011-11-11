c0b3f1141d4e46595660cb7285f9a072676f94b4
John Z <east54john@gmail.com>
2011-11-11 06:07:14 -0500

    Finished chapter 6

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,9 @@
 module ApplicationHelper
-  
+
+  def logo
+     image_tag "images.jpg", :alt => "JZ Sample App", :class => "round"
+  end
+
   def myhelper
     base_title = "Ruby on Rails Tutorial Sample App"
     if@title.nil?
---
+++ spec/models/user_spec.rb
@@ -0,0 +1,48 @@
+require 'spec_helper'
+
+describe User do
+  before(:each) do
+    @attr = {:name => "Example User", :email => "joo@yooo.com"}
+  end
+
+  it "should have non blank name" do
+    no_name_user = User.new(@attr.merge(:name => ""))
+    no_name_user.should_not be_valid
+  end
+
+  it "should have non empty email" do
+    no_email_user = User.new(@attr.merge(:email => ""))
+    no_email_user.should_not be_valid
+  end
+
+  it "name should be within 50 char long" do
+    long_name = "a" * 51;
+    long_name_user = User.new(@attr.merge(:name => long_name))
+    long_name_user.should_not be_valid
+  end
+
+  it "should approve valid email address" do
+    emails = ["abc123@gmail.com", "ew_faf12@gwe12.org"]
+    emails.each do |address|
+      valid_email_user = User.new(@attr.merge(:email => address))
+      valid_email_user.should be_valid
+    end
+  end
+
+  it "should not approve invalid email address" do
+    emails = %w[abc123~@gmail.com, ewe$wefw2@we21.org]
+    emails.each do |address|
+      valid_email_user = User.new(@attr.merge(:email => address))
+      valid_email_user.should_not be_valid
+    end
+  end
+
+  it "should reject duplicate emails up to case" do 
+    upcased_email = @attr[:email].upcase
+    User.create!(@attr.merge(:email => upcased_email))
+    user_with_duplicate_email = User.new(@attr)
+    user_with_duplicate_email.should_not be_valid
+    end
+
+
+end
