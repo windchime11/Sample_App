54290d6d5679480b112539ba390e89c9c5c93928
John Z <east54john@gmail.com>
2011-12-20 14:30:30 -0500

    ajax done

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -1,8 +1,30 @@
 class RelationshipsController < ApplicationController
+
+  before_filter :authenticate
+
   def create
+    @user = User.find(params[:relationship][:followed_id])
+    current_user.follow!(@user)
+    respond_to do |format|
+       format.html {redirect_to @user}
+       format.js
+    end
+  
   end
 
   def destroy
+    #tried session[:return_to] = request.fullpath
+    #      redirect_to(session[:return_to])
+    #It does not work!
+    #Code to find @user is different in destroy from in create
+    @user = Relationship.find(params[:id]).followed
+    current_user.unfollow!(@user)
+    respond_to do |format|
+        format.html {redirect_to @user}
+        format.js
+    end
+    
   end
 
+  
 end
--- spec/controllers/relationships_controller_spec.rb
+++ spec/controllers/relationships_controller_spec.rb
@@ -2,18 +2,76 @@ require 'spec_helper'
 
 describe RelationshipsController do
 
-  describe "GET 'create'" do
-    it "should be successful" do
-      get 'create'
-      response.should be_success
+  before(:each) do
+    @user =Factory(:user)
+  end
+
+
+  describe "access control" do
+    it "can not create without signin" do
+      post :create
+      response.should redirect_to(signin_path) 
+    end
+
+    it "can not destroy without signin" do
+      delete :destroy, :id => 1
+      response.should redirect_to(signin_path) 
     end
   end
 
-  describe "GET 'destroy'" do
-    it "should be successful" do
-      get 'destroy'
-      response.should be_success
+  #<Test for follow/unfollow button
+  describe "POST create" do
+    
+    before(:each) do
+      test_sign_in(@user)
+      @followed = Factory(:user, :email => Factory.next(:email))
     end
+
+    it "can create after sign in" do
+      lambda do
+        post :create, :relationship => {:followed_id => @followed}
+        response.should be_redirect
+      end.should change(Relationship,:count).by(1) 
+    end
+    
+    it "should create using ajax" do
+      lambda do
+        xhr :post, :create, :relationship => {:followed_id => @followed}
+        response.should be_success
+      end.should change(Relationship, :count).by(1)
+    end
+
   end
+
+  describe "DELETE destroy" do
+
+    before(:each) do
+      test_sign_in(@user)
+      @followed = Factory(:user, :email => Factory.next(:email)) 
+      @user.follow!(@followed)
+      @relationship = @user.relationships.find_by_followed_id(@followed)
+   end
+
+
+    it "can destroy after signin" do
+      lambda do
+      delete :destroy, :id => @relationship
+      response.should be_redirect 
+      end.should change(Relationship, :count).by(-1)
+    end
+
+
+    it "can destroy after signin using ajax" do
+      lambda do
+      xhr :delete, :destroy, :id => @relationship
+      response.should be_success
+      end.should change(Relationship, :count).by(-1)
+    end
+
+  end
+
+  #Test for follow/unfollow button>
+
+
 
 end
